2afabc6235919ebc9188951b95280c854fdb8f6a
Antoine Delcourte <adelcourte.pro@gmail.com>
2023-05-12 09:29:24 +0200

    Creating Tag entity

---
+++ src/Entity/Tag.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TagRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TagRepository::class)]
+class Tag
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $icon = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\ManyToMany(targetEntity: TagCategory::class, inversedBy: 'tags')]
+    private Collection $tag_category_id;
+
+    public function __construct()
+    {
+        $this->tag_category_id = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIcon(): ?string
+    {
+        return $this->icon;
+    }
+
+    public function setIcon(string $icon): self
+    {
+        $this->icon = $icon;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, TagCategory>
+     */
+    public function getTagCategoryId(): Collection
+    {
+        return $this->tag_category_id;
+    }
+
+    public function addTagCategoryId(TagCategory $tagCategoryId): self
+    {
+        if (!$this->tag_category_id->contains($tagCategoryId)) {
+            $this->tag_category_id->add($tagCategoryId);
+        }
+
+        return $this;
+    }
+
+    public function removeTagCategoryId(TagCategory $tagCategoryId): self
+    {
+        $this->tag_category_id->removeElement($tagCategoryId);
+
+        return $this;
+    }
+}
